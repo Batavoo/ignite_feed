8d1dd2b2adf8efcc334a9432ce3b45cf53f0026b
gustavo <gsarruda.office@gmail.com>
2024-02-20 16:38:42 -0300

    testando

---
+++ src/components/Comment.jsx
@@ -0,0 +1,40 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/Batavoo.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gustavo Santos</strong>
+              <time
+                title="19 de Janeiro às 08:13h"
+                dateTime="2024-01-19 08:13:30"
+              >
+                Cerca de 1hr atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
